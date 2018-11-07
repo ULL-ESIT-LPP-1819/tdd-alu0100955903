0ceec6c25ce15998c3ac5f6a4424e33b91f70b12
Liam Okelly <alu0100955903@ull.edu.es>
2018-11-07 18:11:27 +0000

    Test 1

--- lib/prct06.rb
+++ lib/prct06.rb
@@ -1,6 +1,16 @@
-require "prct06/version"
-
-module Prct06
-  class Error < StandardError; end
+#require "prct06/version"
+#
+#module Prct06
+ # class Error < StandardError; end
   # Your code goes here...
+#end
+#
+class Etiqueta
+
+	attr_reader :saturadas, :monoinsaturadas, :polinsaturadas, :azucares, :polialcoles, :almidon, :fibra, :proteinas, :sal
+	
+
+	def initialize (saturadas, monoinsaturadas, polinsaturadas, azucares, polialcoles, almidon, fibra, proteinas, sal)
+		@saturadas, @monoinsaturadas, @polinsaturadas, @azucares, @polialcoles, @almidon, @fibra, @proteinas, @sal = saturadas, monoinsaturadas, polinsaturadas, azucares, polialcoles, almidon, fibra, proteinas, sal
+	end
 end
