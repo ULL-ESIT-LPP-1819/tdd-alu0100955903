e327cb52cf4b19de6914709e50a97c6b778b2841
Liam Okelly <alu0100955903@ull.edu.es>
2018-11-14 15:30:09 +0000

    [Test 6]: Hacemos la funcion insertar [ACIERTO]

--- lib/prct06/lista.rb
+++ lib/prct06/lista.rb
@@ -9,4 +9,23 @@ class List
 		@tail = tail
 	end
 
+        def insertar_por_cola(nodo)
+                if(@tail==NIL)
+                        @tail=nodo
+                        @head=nodo
+                else
+                        nodo.prev=@tail
+                        @tail.nest=nodo
+                        @tail=nodo
+                end
+        end
+
+        def extraer_por_cabeza(nodo)
+
+
+        end
+
+        def vacio
+        end
+
 end
