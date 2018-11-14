c4a2f2e6eebd26093d75a37512a0b1a0b992302a
Liam Okelly <alu0100955903@ull.edu.es>
2018-11-14 10:30:41 +0000

    [Test 1]: Creamos el lista_spec.rb y declaramos dos nodos. [ERROR]

---
+++ spec/lista_spec.rb
@@ -0,0 +1,9 @@
+require 'prct06/prct06.rb'
+require 'prct06/lista'
+
+RSpec.describe Etiqueta do
+        before :each do #Inicializamos los datos
+		@nodo1 = Node.new(1,NIL,NIL)
+		@nodo2 = Node.new(2,NIL,NIL)
+        end
+
