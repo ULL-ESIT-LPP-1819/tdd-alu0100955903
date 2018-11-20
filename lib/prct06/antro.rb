01eedcf4cf735ef180702b2b502527d8de162fb4
Liam Okelly <alu0100955903@ull.edu.es>
2018-11-20 15:43:19 +0000

    [Test 4]: Creamos las pruebas de la clase antropometrico

---
+++ lib/prct06/antro.rb
@@ -0,0 +1,3 @@
+class Antropometrico
+
+end
--- spec/antro_spec.rb
+++ spec/antro_spec.rb
@@ -1,11 +1,12 @@
 require 'prct06/lista'
 require 'prct06/prct06'
-#require 'prct06/persona'
+require 'prct06/antro'
 
 RSpec.describe Prct06 do
         before :each do
                 @l1 = List.new(nil,nil)
 		@e1 =Etiqueta.new("Galletas",10,10,10,10,10,10,10,10,10)
+		@a1 =Antropometrico.new(70,80)
         end
 
         describe "Pruebas de lista" do
@@ -37,5 +38,20 @@ RSpec.describe Prct06 do
                         expect(@e1.is_a? Etiqueta).to eq(true)
                 end
         end
+
+	describe "Pruebas de Antropometrico" do
+		
+		it "La clase antropometrico existe" do
+			expect(@a1.class).to eq(Antropometrico)
+		end
+		
+		it "Es una instancia de la clase antropometrico" do
+			expect(@a1.instance_of? Antropometrico).to eq(true)
+		end
+
+		it "Es un objeto de la clase antropometrico" do
+			expect(@a1.is_a? Antropometrico).to eq(true)
+		end
+	end
 end
 
