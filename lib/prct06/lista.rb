29359312b68b46f66e4cff2f6863fca1ac49e2c7
Liam Okelly <alu0100955903@ull.edu.es>
2018-12-18 17:37:09 +0000

    Haciendo funcion each y for para lista

--- lib/prct06/lista.rb
+++ lib/prct06/lista.rb
@@ -116,4 +116,48 @@ class List
 		end
 	end
 
+        def ordenacion_for
+                @resultado = self.map { |x| x }
+                for x in 0..self.count-1
+                        var = @resultado[x]
+                        i = x
+
+                        for y in x + 1..self.count-1
+                                if var > @resultado[y]
+                                        var = @resultado[y]
+                                        i = y
+                                end
+                        end
+
+                        @resultado[i] = @resultado[x]
+                        @resultado[x] = var
+                end
+                @resultado
+        end
+
+
+        def ordenacion_each
+                @resultado = self.map { |x| x }
+                indice=0
+                @resultado.each do |x|
+                        var = x
+                        i = indice
+                        indice2=indice + 1
+
+                        @resultado[indice2..@resultado.length-1].each do |y|
+                                if var > y
+                                        var = y
+                                        i = indice2
+                                end
+                                indice2+=1
+                        end
+
+                        @resultado[i] = x
+                        @resultado[indice] = var
+
+                        indice+=1
+                end
+                @resultado
+        end
+
 end
